9c3f763142006598110d956b366f3d2f16abe22e
Nadia080290 <ngallardome@gmail.com>
2020-09-26 20:02:07 -0300

    navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Home = () => {
     return (
         <div>
+            <Navbar/>
             <h1>SOY EL HOME</h1> 
         </div>
     )
