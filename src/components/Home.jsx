5cb2f096efb16d1242263954cfde5cf7c4484a87
pmvaldez <valdezp69@gmail.com>
2020-09-26 20:02:35 -0300

    inicio footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import FooterPage from './FooterPage'
 
 const Home = () => {
     return (
         <div>
             <h1>SOY EL HOME</h1> 
+            <FooterPage />
         </div>
     )
 }
