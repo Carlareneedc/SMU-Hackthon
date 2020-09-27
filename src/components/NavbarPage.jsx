a223da1716a4d27493e60302318dedea67ed64f2
Carlareneedc <carla.renee.diazc@gmail.com>
2020-09-26 22:44:01 -0300

    nav

--- src/components/NavbarPage.jsx
+++ src/components/NavbarPage.jsx
@@ -1,14 +1,13 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-
 import React from 'react';
 import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBMask, MDBView } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
 import especie from '../img/bckgrounduno.png'
 import logo from '../img/logounimarc.png'
 import mote from '../img/moteconhuesillo.png'
-
+import { MDBBtn } from "mdbreact";
 class FullPageIntroWithFixedNavbar extends React.Component {
   constructor(props) {
     super(props);
@@ -18,13 +17,11 @@ class FullPageIntroWithFixedNavbar extends React.Component {
     };
     this.onClick = this.onClick.bind(this);
   }
-
   onClick() {
     this.setState({
       collapse: !this.state.collapse
     });
   }
-
   render() {
     return (
       <div>
@@ -59,18 +56,18 @@ class FullPageIntroWithFixedNavbar extends React.Component {
               </MDBCollapse>
             </MDBNavbar>
           </Router>
-
           <MDBView src={especie}>
             <MDBMask overlay="black-light" className="flex-center flex-column text-white text-center">
               <h1>Receta del mes</h1>
               <h5>¿Mote con Huesillo? Si que si. Comencemos la celebracion con un rico motesito helado.</h5>
               <br />
               <p>Que ha tu fonda no le falte nada, con el rico sabor de esta tradicional bebida chile.</p>
+              <MDBBtn rounded color="danger">Receta del Mes</MDBBtn>
+              <MDBBtn rounded outline color="danger">Más Recetas</MDBBtn>
             </MDBMask>
-           {/*  <img src={mote}/> */}
+            <img src={mote}/>
           </MDBView>
         </header>
-
         <main>
           <MDBContainer className="text-center my-5">
             <p align="justify">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -80,5 +77,4 @@ class FullPageIntroWithFixedNavbar extends React.Component {
     );
   }
 }
-
 export default FullPageIntroWithFixedNavbar;
